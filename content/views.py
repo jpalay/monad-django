ec412c997479cfe0266b6849fbb844ff04dd4b35
Adam <adampalay@gmail.com>
2013-08-31 17:25:12 -0400

    initial commit of views

--- content/views.py
+++ content/views.py
@@ -1,1 +1,19 @@
 # Create your views here.
+
+from content.models import *
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+	return render_to_response("landing.html")
+
+@login_required
+def display_all(request):
+	pages = Page.objects.all().order_by('order')
+	render_to_response("display_all.html", pages)
+
+@login_required
+def display_one(request, slug):
+	page = Page.objects.filter(slug=slug)[0]
+	render_to_response("display_one.html", page)
