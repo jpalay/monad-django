3e4d8844578cb829c25a082eb05be3ba98f4c605
Adam <adampalay@gmail.com>
2013-09-01 14:40:43 -0400

    uncomment import thing

--- monad/settings.py
+++ monad/settings.py
@@ -180,8 +180,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# try:
-#     from local_settings import *
-# except ImportError:
-#     pass
+try:
+    from local_settings import *
+except ImportError:
+    pass
 
